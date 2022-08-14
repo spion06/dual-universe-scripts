7c8e7cd84ea296390777b9cb30de38c6c0ce8a6f
spion <spion06@gmail.com>
2022-08-14 02:58:17 -0700

    add in basic script for generating a dot graph for recipes

---
+++ python/recipe-calculator/README.md
@@ -0,0 +1,16 @@
+# recipe-calculator
+
+This is a basic python script to deal with recipes in dual-universe. WORK-IN-PROGRESS
+
+currently supports generating a dot file (usable with graphviz) for seeing a DAG of what is needed to build a product
+
+## requirements
+
+```
+pip install -r requirements.txt
+```
+
+## usage
+```
+python create_dot.py [ITEM_NAME]
+```
---
+++ python/recipe-calculator/create_dot.py
@@ -0,0 +1,110 @@
+import yaml
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('items', metavar='ITEM', type=str, nargs='*', default=[],
+                    help='what items out output the dot graph for')
+parser.add_argument('--file', default="recipes.yaml",
+                    help='file name to use for recipe list')
+parser.add_argument('--output', default="graph.dot",
+                    help='file name to output dot file into')
+
+args = parser.parse_args()
+
+with open(args.file) as f:
+    doc = yaml.safe_load(f.read()) #type: dict
+
+dot_graphs = [] #type: list[str]
+made_in_list = {} #type: dict[str,list[str]]
+nodes = [] #type: list[str]
+edges = [] #type: list[list[str]]
+
+def create_made_in_node(item:str, made_in:str):
+    return f"{item}_{made_in}"
+
+def generate_nodes(doc:dict[str,dict]):
+    for item, vals in doc.items():
+        made_in = vals.get("made_in")
+        if made_in:
+            if made_in not in made_in_list:
+                made_in_list[made_in] = []
+            made_in_list[made_in].append(item)
+        nodes.append(item)
+        ingredients = vals.get("ingredients", [])
+        for ingredient in ingredients:
+            if ingredient not in nodes:
+                nodes.append(ingredient)
+
+def render_nodes():
+    for node in nodes:
+        yield f"{node} [color=turquoise];"
+    yield ""
+    for made_in, items in made_in_list.items():
+        yield f"node[label=\"build_in_{made_in}\"];"
+        for item in items:
+            yield create_made_in_node(item, made_in) + ";"
+        yield ""
+
+def create_graph_for_item(item, vals):
+    if not vals:
+        return ()
+    made_in = vals.get("made_in")
+    if not made_in:
+        return () # if there is no associated made_in don't generate an item
+    dag = []
+    ingredients = vals.get("ingredients", {}) #type: map[str,int]
+    dag.append("{%s}" % (",".join(ingredients)))
+    dag.append(create_made_in_node(item, made_in))
+    dag.append(item)
+    return (dag, ingredients)
+    
+def list_diff(list1, list2):
+    diff = []
+    for list_item in list1:
+        if list_item not in list2:
+            diff.append(list_item)
+    return diff
+
+def create_graph_for_items(itemids:list[str], all_items=[]):
+    dags = []
+    for item in itemids:
+        if item in all_items:
+            continue
+        if item not in doc:
+            continue
+        dag, ingredients = create_graph_for_item(item, doc[item])
+        dags.append(" -> ".join(dag) + ";")
+        all_items.append(item)
+        if len(ingredients.keys()) > 0:
+            ingredient_dags, items_for_ingredients = create_graph_for_items(list(ingredients.keys()), all_items)
+            dags += ingredient_dags
+    return (dags, all_items)
+    
+dags = []
+if len(args.items) == 0:
+    for item, values in doc.items():
+        dags.append(" -> ".join(create_graph_for_item(item, values)[0]) + f";")
+else:
+    items_to_parse = set(args.items)
+    items_in_doc = set(doc.keys())
+    list_differences = list_diff(items_to_parse, items_in_doc)
+    if len(list_differences) > 0:
+        raise argparse.ArgumentError(f"invalid items passed {','.join(list_differences)}")
+    dags, all_items = create_graph_for_items(items_to_parse)
+    all_items = set(all_items)
+    for doc_item in items_in_doc:
+        if doc_item not in all_items:
+            doc.pop(doc_item, None)
+
+
+generate_nodes(doc)
+with open(args.output, "w") as output_file:
+    output_file.write("digraph simple {\n")
+    for item in render_nodes():
+        output_file.write(item + "\n")
+    for dag in dags:
+        output_file.write(dag + "\n")
+    output_file.write("}")
+print(f"wrote dag to {args.output}")
+
+
